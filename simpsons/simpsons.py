ee3598a665cf166d3e9fd025a045a7430c8dcae5
Nathan Bjork <nbjork@gmail.com>
2018-06-08 14:57:07 -0500

    Update simpsons.py

--- simpsons/simpsons.py
+++ simpsons/simpsons.py
@@ -1,1 +1,14 @@
+from discord.ext import commands
+import compuglobal
 
+class Simpsons:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.command()
+async def sgif(self):
+    simpsons = compuglobal.Frinkiac()
+    screencap = simpsons.get_random_screencap()
+    gif = await screencap.get_gif_url()
+    self.bot.say(gif)
