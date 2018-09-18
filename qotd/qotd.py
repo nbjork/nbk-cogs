686b2f85a8df6150a6e370e27aa73e80b7c6a5ff
Nathan Bjork <nbjork@gmail.com>
2018-09-18 18:59:56 -0500

    Update qotd.py

--- qotd/qotd.py
+++ qotd/qotd.py
@@ -3,14 +3,15 @@ import wikiquote
 
 
 class Qotd:
-    
+
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def qotd(self):
+     @commands.command()
+     async def qotd(self):
 
-        qotd = wikiquote.quote_of_the_day() # returns a (quote, author) tuple
+        qotd = wikiquote.quote_of_the_day()
         await self.bot.say(qotd[0])
+
 def setup(bot):
     bot.add_cog(Qotd(bot))
