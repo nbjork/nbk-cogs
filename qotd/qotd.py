ab74cc7413802360c235d61822bad3e0520412d2
Nathan Bjork <nbjork@gmail.com>
2018-09-18 18:50:16 -0500

    Create qotd.py

---
+++ qotd/qotd.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import wikiquote
+
+
+class Qotd:
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def qotd(self):
+
+        qotd = wikiquote.quote_of_the_day() # returns a (quote, author) tuple
+        await self.bot.say(qotd[0])
+def setup(bot):
+    bot.add_cog(Qotd(bot))
