39486104884416e6fad87f41188ee24e4852963a
Nathan Bjork <nbjork@gmail.com>
2018-09-20 11:33:40 -0500

    Update qotd.py

--- qotd/qotd.py
+++ qotd/qotd.py
@@ -16,7 +16,10 @@ class Qotd:
         qotd = wikiquote.quote_of_the_day()
         await self.bot.say(qotd[0])
         await self.bot.say("- " + qotd[1])
-
+        
+    async def qsearch(self, sterm):
+        search = wikiquote.quotes(sterm,max_quotes=3)
+        await self.bot.say(search)
 
 def setup(bot):
     bot.add_cog(Qotd(bot))
